92eb045e96b47d5bec7713e22c492cc399f0e4e7
Vishva066 <vishvaprasad.p.2019.cse@rajalakshmi.edu.in>
2025-02-09 09:02:17 +0530

    🚚 || Added resources RAM constraints

--- harhdat/ignition/modules/Cert.js
+++
@@ -1,6 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("CertModule", (m) => {
-    const cert = m.contract("Cert");
-    return { cert };
-});
